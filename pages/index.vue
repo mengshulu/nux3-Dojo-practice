5dbf135471e7c52cdb4cfe790e6c6c9671d31efc
shu <shu.lu0316@gmail.com>
2024-06-07 17:12:04 +0800

    feat: add  catalog

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,83 @@
 <template>
   <div>
-    <h2>Home</h2>
-    <p>電視台演唱會年輕人，真實苗栗開展，開通一是，製品十二處於各地陷入只要商務，夫妻註冊時間告訴我頁面光碟筆記本，上路重量普通還能你自己專區破解圖片學生就像，浙江簡歷剛才學會分析花園資源，美容航空事業，很。</p>
+    <h1>目錄</h1>
+    <hr>
+    <div class="catalog">
+      <ul
+        v-for="(item, index) in list"
+        :key="`catalog_${index}`"
+        class="content"
+      >
+        <NuxtLink :to="item.id">
+          <li class="title">
+            <h2>{{ item.name }}</h2>
+            <ul>
+              <li class="description"><p>{{ item.description }}</p></li>
+            </ul>
+          </li>
+        </NuxtLink>
+      </ul>
+    </div>
+    <!-- <p>電視台演唱會年輕人，真實苗栗開展，開通一是，製品十二處於各地陷入只要商務，夫妻註冊時間告訴我頁面光碟筆記本，上路重量普通還能你自己專區破解圖片學生就像，浙江簡歷剛才學會分析花園資源，美容航空事業，很。</p>
     <p>要商務，夫妻註冊時間告訴我頁面光碟筆記本，上路重量普通還能你自己專區破解圖片學生就像，浙江簡歷剛才學會分析花園資源，美容航空事業，很。
   招生認證的是證件戰爭採用管理系統是因為和他利益收到，禁止工藝種類不願意英文億元說出設定模糊，不停答案調節說過領先我會，個性實用刪除消失喜愛手術死了網際快車顯然，擔心運動版面新人要是住房，同意遇到留言板您可以基礎上來電舉辦世界盃目錄尺寸旅遊，榮譽因素必須一條加拿大綜合筆者，受到需要一隻嘉義都會規律對外到了學科狀態阿摩斯，維持及時必然吃了一臉思考加盟工程，世界上電腦一身就是，要是是這樣健康天使，工程。</p>
-  <p>控制損失有很多內容魯蛇萬元取消電信，群眾連載情節楠雅花園加強電子能在對於更多重要執行環境，老婆一家台北會議商品，阿宅人民共和國顯得部分本月本書也可總體名稱故障每天將軍留下，我把接近成果我想警察，報價。</p>
+  <p>控制損失有很多內容魯蛇萬元取消電信，群眾連載情節楠雅花園加強電子能在對於更多重要執行環境，老婆一家台北會議商品，阿宅人民共和國顯得部分本月本書也可總體名稱故障每天將軍留下，我把接近成果我想警察，報價。</p> -->
+  <!-- <p>Q: Error: Cannot find name 'xxx' ts 2304</p>
+  <p>A: My tsconfig.json:
+    <pre>
+    <code>
+      {
+
+        "extends": "./.nuxt/tsconfig.json",
+        "include": [
+          "env.d.ts",
+          ".nuxt/nuxt.d.ts",
+          "**/*"
+        ]
+      }
+    </code>
+    </pre>
+  </p> -->
   </div>
 </template>
 
-<script setup>
-
+<script lang="ts" setup>
+  interface LIST {
+    id: string;
+    name: string;
+    description: string;
+  };
+  const list = ref([
+    {
+      id: 'map',
+      name: 'Leaflet',
+      description: '使用 Leaflet 繪製台灣地圖'
+    }
+  ] as LIST[]);
 </script>
 
-<style scoped>
-  h2 {
-    margin-bottom: 20px;
-    font-size: 36px;
-  }
-  p {
-    margin: 20px 0;
+<style scoped lang="scss">
+  .catalog {
+    margin-top: var(--m-space);
+
+    .content {
+      border-radius: var(--s-space);
+      transition: background .4s, color .2s;
+      
+      &:hover {
+        color: #fff;
+        background: var(--bg-hover);
+      }
+    }
+
+    .title {
+      margin-left: var(--l-space);
+      list-style: auto;
+    }
+
+    .description {
+      margin-left: var(--s-space);
+      list-style: circle;
+    }
   }
 </style>

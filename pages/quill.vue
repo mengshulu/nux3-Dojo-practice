acab6f3eaead07599d661e7c739047b1201ad164
shu <shu.lu0316@gmail.com>
2024-06-10 23:33:09 +0800

    delete quill v1

--- pages/quill.vue
+++ pages/quill.vue
@@ -2,49 +2,49 @@
   <h2>
     Quill
   </h2>
-  <QuillEditor :options='options' />
+  <!-- <QuillEditor :options='options' /> -->
 </template>
 
 <script setup lang="ts">
-  import { QuillEditor } from '@vueup/vue-quill';
-  import '@vueup/vue-quill/dist/vue-quill.snow.css';
-  import BlotFormatter from 'quill-blot-formatter';
+  // import { QuillEditor } from '@vueup/vue-quill';
+  // import '@vueup/vue-quill/dist/vue-quill.snow.css';
+  // import BlotFormatter from 'quill-blot-formatter';
 
-  const toolbarOptions = [
-    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-    ['blockquote', 'code-block'],
-    ['link', 'image', 'video', 'formula'],
+  // const toolbarOptions = [
+  //   ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  //   ['blockquote', 'code-block'],
+  //   ['link', 'image', 'video', 'formula'],
 
-    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-    [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
-    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-    [{ 'direction': 'rtl' }],                         // text direction
+  //   [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+  //   [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
+  //   [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+  //   [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+  //   [{ 'direction': 'rtl' }],                         // text direction
 
-    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  //   [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+  //   [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
 
-    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-    [{ 'font': [] }],
-    [{ 'align': [] }],
+  //   [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+  //   [{ 'font': [] }],
+  //   [{ 'align': [] }],
 
-    ['clean']                                         // remove formatting button
-  ];
+  //   ['clean']                                         // remove formatting button
+  // ];
 
-  const options = {
-    debug: 'info',
-    modules: {
-      toolbar: toolbarOptions,
-      blotFormatter: {
-        name: 'blotFormatter',  
-        module: BlotFormatter, 
-        options: {/* options */}
-      }
-    },
-    placeholder: '輸入一些內容吧',
-    // readOnly: true,
-    theme: 'snow'
-  };
+  // const options = {
+  //   debug: 'info',
+  //   modules: {
+  //     toolbar: toolbarOptions,
+  //     blotFormatter: {
+  //       name: 'blotFormatter',  
+  //       module: BlotFormatter, 
+  //       options: {/* options */}
+  //     }
+  //   },
+  //   placeholder: '輸入一些內容吧',
+  //   // readOnly: true,
+  //   theme: 'snow'
+  // };
 
 
 </script>

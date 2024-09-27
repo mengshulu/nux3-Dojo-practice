ea19cf91a03421ba3cf89501809e243196f4c11b
shu <shu.lu0316@gmail.com>
2024-09-27 23:40:08 +0800

    feat: add font

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,6 +11,11 @@
           <li><NuxtLink to="/about" class="hover:text-zinc-500">About</NuxtLink></li>
           <li><NuxtLink to="/count" class="hover:text-zinc-500">Count {{ count }}</NuxtLink></li>
           <li><NuxtLink to="/products" class="hover:text-zinc-500">Products</NuxtLink></li>
+          <li><NuxtLink to="/fontToPNG" class="hover:text-zinc-500">
+            <span class="material-symbols-outlined">
+            image
+            </span>
+          </NuxtLink></li>
           <!-- <li><NuxtLink to="/products/abc">404</NuxtLink></li> -->
           <li v-if="!auth.isAuthenticate"><NuxtLink to="/login"  class="hover:text-zinc-500">Login</NuxtLink></li>
           <li v-else class="font-bold text-green-500"><NuxtLink to="/user"  class="hover:text-zinc-500">User</NuxtLink></li>

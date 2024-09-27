5d34f6b5f5646bb77d355e161500ceff15b98770
shu <shu.lu0316@gmail.com>
2024-09-27 21:14:36 +0800

    feat: update product content

--- components/ProductDetails.vue
+++ components/ProductDetails.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="card">
-    <div class="grid grid-cols-2 gap-10">
-      <div class="p-7">
+    <div class="grid grid-cols-1 gap-3 lg:grid-cols-2 lg:gap-10">
+      <div class="p-4 lg:p-7">
         <img :src="product.image" alt="product img" class="mx-auto my-7">
       </div>
-      <div class="p-7">
-        <h2 class="text-4xl my-7">{{ product.title }}</h2>
-        <p class="text-xl my-7">Price - ${{ product.price }}</p>
+      <div class="p-4 lg:p-7">
+        <h2 class="text-4xl my-4 lg:my-7">{{ product.title }}</h2>
+        <p class="text-xl my-4 lg:my-7">Price - ${{ product.price }}</p>
         <h3 class="fint-bold border-b-2 mb-4 pb-2">Product description:</h3>
-        <p class="mb-7">{{ product.description }}</p>
+        <p class="mb-4 lg:mb-7">{{ product.description }}</p>
         <button class="btn flex">
           <i class="material-icons mr-2">add_shopping_cart</i>
           <span>Add to cart</span>
